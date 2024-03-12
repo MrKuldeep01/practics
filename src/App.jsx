6cab460b7c5c8edfc27621674c918801c31bcfc8
Mr. Kuldeep <128669361+MrKuldeep01@users.noreply.github.com>
2024-03-12 19:51:01 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Colorbtn from './components/colorbtn.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Colorbtn from "./components/colorbtn.jsx";
 function App() {
   return (
     <>
-    <div className="background">
-      <ul className="colortab">
-        <Colorbtn colorname={"red"}/>
-        <Colorbtn colorname={"blue"}/>
-        <Colorbtn colorname={"deepPink"}/>
-        <Colorbtn colorname={"green"}/>
-        <Colorbtn colorname={"crimson"}/>
-        <Colorbtn colorname={"royalBlue"}/>
-        <Colorbtn colorname={"gray"}/>
-        <Colorbtn colorname={"skyBlue"}/>
-        <Colorbtn colorname={"salmon"}/>
-        <Colorbtn colorname={"aqua"}/>
-        <Colorbtn colorname={"coral"}/>
-        <Colorbtn colorname={"maroon"}/>
-      </ul>
-      <h2 className='heading'>Click buttons to flip Background</h2>
-    </div>
+      <div className="background">
+        <ul className="colortab">
+          <Colorbtn colorname={"deepPink"} />
+          <Colorbtn colorname={"royalBlue"} />
+          <Colorbtn colorname={"gray"} />
+          <Colorbtn colorname={"burlywood"} />
+          <Colorbtn colorname={"cadetblue"} />
+          <Colorbtn colorname={"chocolate"} />
+          <Colorbtn colorname={"coral"} />
+          <Colorbtn colorname={"crimson"} />
+          <Colorbtn colorname={"cyan"} />
+          <Colorbtn colorname={"darkgreen"} />
+          <Colorbtn colorname={"skyBlue"} />
+          <Colorbtn colorname={"salmon"} />
+          <Colorbtn colorname={"aqua"} />
+          <Colorbtn colorname={"coral"} />
+          <Colorbtn colorname={"maroon"} />
+        </ul>
+        <h2 className="heading">Click buttons to flip Background</h2>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
